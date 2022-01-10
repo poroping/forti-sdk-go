6a3119bccdf3a6322b15ad639a8ae4669adc3fe5
poroping <43107292+poroping@users.noreply.github.com>
2022-01-10 13:17:37 +0000

    remove unused models

--- v2/models/router.bgp_redistribute.go
+++
@@ -1,9 +0,0 @@
-package models
-
-const RouterbgpRedistributePath = "router/bgp/redistribute/"
-
-type RouterbgpRedistribute struct {
-	Name     *string `json:"name,omitempty"`
-	RouteMap *string `json:"route-map,omitempty"`
-	Status   *string `json:"status,omitempty"`
-}
--- v2/models/system.sdwan_zone.go
+++
@@ -1,8 +0,0 @@
-package models
-
-const SystemsdwanZonePath = "system/sdwan/zone/"
-
-type SystemsdwanZone struct {
-	Name               *string `json:"name,omitempty"`
-	ServiceSlaTieBreak *string `json:"service-sla-tie-break,omitempty"`
-}
