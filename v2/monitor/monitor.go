5722b851a9706cd37050802de501a688e1a3e0fc
poroping <43107292+poroping@users.noreply.github.com>
2022-01-11 11:55:33 +0000

    add some monitor endpoints;

--- v2/monitor/monitor.go
+++ v2/monitor/monitor.go
@@ -4,13 +4,17 @@ import (
 	"github.com/poroping/forti-sdk-go/v2/config"
 )
 
-func New(config config.Config) Endpoints {
+func New(config *config.Config) Endpoints {
 	return &Client{config: config}
 }
 
 type Client struct {
-	config config.Config
+	config *config.Config
 }
 
 type Endpoints interface {
+	MonitorRouterBgpNeighbors() (*[]MonitorRouterBgpNeighborsResult, error)
+	MonitorRouterBgpPaths() (*[]MonitorRouterBgpPathsResult, error)
+	MonitorRouterOspfNeighbors() (*[]MonitorRouterOspfNeighborsResult, error)
+	MonitorRouterIPv4() (*[]MonitorRouterIPv4Result, error)
 }
